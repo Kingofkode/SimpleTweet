a0633255dfd2da1901ae8881e271980526b6f81a
Isaiah Suarez <isaiahsuarez@fb.com>
2020-06-30 13:12:29 -0700

    Added the ability to publish a tweet on backend.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,18 +3,30 @@ package com.codepath.apps.restclienttemplate;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
+
 public class ComposeActivity extends AppCompatActivity {
+
+    private static final String TAG = "ComposeActivity";
 
     public static final int MAX_TWEET_LENGTH = 140;
 
     EditText etCompose;
     Button btnTweet;
+
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,32 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         getSupportActionBar().hide();
 
+        client = TwitterApp.getRestClient(this);
+
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
         
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String tweetContent = etCompose.getText().toString();
+                final String tweetContent = etCompose.getText().toString();
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSuccess: to publish tweet");
+                        try {
+                            Tweet publishedTweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet says: " + publishedTweet);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure: to publish tweet", throwable);
+                    }
+                });
             }
         });
 
