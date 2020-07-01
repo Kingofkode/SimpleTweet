03cd51bc7e453a7b9b857869aece79f7ac4807af
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-01 11:43:10 -0700

    Used tool bar to add twitter icon to action bar.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -34,7 +34,6 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
-        getSupportActionBar().hide();
 
         client = TwitterApp.getRestClient(this);
 
